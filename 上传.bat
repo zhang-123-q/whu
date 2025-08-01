01dd60299a28112750e21b2b552b114bb21c004a
zhang-123-q <18250984086@163.com>
2025-08-01 19:36:25 +0800

    鑷姩鎻愪氦锛?datetime

--- 上传.bat
+++ 上传.bat
@@ -1,12 +1,11 @@
 @echo off
-:: 切换到项目目录（如果脚本不在项目目录）
 cd /d "D:\1-编程\wdu"
 
-:: Git 操作
+
 git add .
 git commit -m "自动提交：%date% %time%"
 git push origin main
 
-:: 可选：完成后提示
+
 echo 代码已上传至GitHub！
 pause
